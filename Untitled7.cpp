7bcb0426e5cc4daebb11c420f4b67d72a388ae84
Mahdi Naji <mahdinaji754@gmail.com>
2024-12-17 10:50:54 +0330

    Create Untitled7.cpp

---
+++ Untitled7.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+using namespace std;
+
+void print ( );
+
+main()
+
+{
+	print();
+	cout<<'*';
+	print();
+}
+
+void print ( )
+{
+	cout<<'A';
+	cout<<'B';
+}
